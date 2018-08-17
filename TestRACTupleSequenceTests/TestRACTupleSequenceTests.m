e5dce94a472d56066ef76a5fabf0f8a04d39a0ed
jiangzenghui005@163.com <jiangzenghui005@163.com>
2018-08-17 18:33:43 +0800

    测试用例提交

--- TestRACTupleSequenceTests/TestRACTupleSequenceTests.m
+++ TestRACTupleSequenceTests/TestRACTupleSequenceTests.m
@@ -8,32 +8,72 @@
 
 #import <XCTest/XCTest.h>
 
+#import <ReactiveCocoa.h>
+#import <RACTupleSequence.h>
+
 @interface TestRACTupleSequenceTests : XCTestCase
 
 @end
 
 @implementation TestRACTupleSequenceTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)test_sequenceWithTupleBackingArray
+{
+    RACTuple *tuple = RACTuplePack(@1, @2, @3);
+    RACSequence *sequence = [RACTupleSequence sequenceWithTupleBackingArray:tuple.allObjects offset:1];
+    NSLog(@"sequenceWithTupleBackingArray -- %@", sequence);
+    
+    // 打印日志；
+    /*
+     2018-08-17 17:40:07.673830+0800 TestRACTupleSequence[52426:18393668] sequenceWithTupleBackingArray -- <RACTupleSequence: 0x600000236600>{ name = , tuple = (
+     1,
+     2,
+     3
+     ) }
+     */
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)test_head
+{
+    RACTuple *tuple = RACTuplePack(@1, @2, @3);
+    RACSequence *sequence = [RACTupleSequence sequenceWithTupleBackingArray:tuple.allObjects offset:1];
+    NSLog(@"head -- %@", sequence.head);
+    
+    // 打印日志；
+    /*
+     2018-08-17 17:41:34.131249+0800 TestRACTupleSequence[52496:18398390] head -- 2
+     */
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)test_tail
+{
+    RACTuple *tuple = RACTuplePack(@1, @2, @3);
+    RACSequence *sequence = [RACTupleSequence sequenceWithTupleBackingArray:tuple.allObjects offset:1];
+    NSLog(@"tail -- %@", sequence.tail);
+    
+    // 打印日志；
+    /*
+     2018-08-17 17:42:32.102848+0800 TestRACTupleSequence[52540:18401646] tail -- <RACTupleSequence: 0x60000022c300>{ name = , tuple = (
+     1,
+     2,
+     3
+     ) }
+     */
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)test_array
+{
+    RACTuple *tuple = RACTuplePack(@1, @2, @3);
+    RACSequence *sequence = [RACTupleSequence sequenceWithTupleBackingArray:tuple.allObjects offset:1];
+    NSLog(@"array -- %@", sequence.array);
+    
+    // 打印日志；
+    /*
+     2018-08-17 17:43:20.283402+0800 TestRACTupleSequence[52589:18404640] array -- (
+     2,
+     3
+     )
+     */
 }
 
 @end
